f2422305c08e782e27c74ee01b23f57553e41593
Jasper van Rooijen <jasper.van.rooijen@hotmail.com>
2015-11-04 13:06:39 +0100

    final final final push

--- src/Controller.java
+++ src/Controller.java
@@ -265,7 +265,7 @@ public class Controller extends Thread implements KeyListener, MouseListener {
         car.updateOrientation(90);
         race.setTracksToGo(1);
         race.setTimeBegin();
-        race.setTracksToGo(3);
+        race.setTracksToGo(2);
         car.getLocation().setChangedSegmentAfterFinish(true);
     }
 
--- src/RunMVC.java
+++ src/RunMVC.java
@@ -16,11 +16,11 @@ public class RunMVC {
     public RunMVC() {
         location = new Location(2,3,31,25);
         location.saveAsInitialLocation();
-        track = new Track(5,4);
+        track = new Track(8,6);
         buildTrack();
         controller = new Controller();
         //I2CProtocol protocol = new I2CProtocol(controller);
-        race = new Race(track, controller, 7, 3);//, protocol);
+        race = new Race(track, controller, 7, 2);//, protocol);
         car = new Car(location, race);
         car.updateOrientation(90);
         view = new GUI(controller);
@@ -47,21 +47,50 @@ public class RunMVC {
         track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 3);
         track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 4);
         track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 5);
-        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 9);
-        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 10);
-        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 14);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 6);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 7);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 8);
         track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 15);
         track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 16);
-        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 17);
         track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 18);
         track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 19);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 23);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 24);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 29);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 31);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 32);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 33);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 34);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 35);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 39);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 40);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 41);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 42);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 43);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 44);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 45);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 46);
+        track.addTrackSegment(new TrackSegmentArray(tsb.getForbidden()), 47);
 
-        track.addTrackSegment(new TrackSegmentArray(tsb.generateRD()), 6);
-        track.addTrackSegment(new TrackSegmentArray(tsb.generateSFLR()), 7);
-        track.addTrackSegment(new TrackSegmentArray(tsb.generateLD()), 8);
-        track.addTrackSegment(new TrackSegmentArray(tsb.generateLR()), 12);
-        track.addTrackSegment(new TrackSegmentArray(tsb.generateRU()), 11);
-        track.addTrackSegment(new TrackSegmentArray(tsb.generateLU()), 13);
+        track.addTrackSegment(new TrackSegmentArray(tsb.generateRD()), 9);
+        track.addTrackSegment(new TrackSegmentArray(tsb.generateSFLR()),10);
+        track.addTrackSegment(new TrackSegmentArray(tsb.generateLR()), 11);
+        track.addTrackSegment(new TrackSegmentArray(tsb.generateLD()), 12);
+        track.addTrackSegment(new TrackSegmentArray(tsb.generateRD()), 13);
+        track.addTrackSegment(new TrackSegmentArray(tsb.generateLD()), 14);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateUD())), 17);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateRU())), 20);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateLU())), 21);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateUD())), 22);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateRU())), 25);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateLR())), 26);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateLR())), 27);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateLD())), 28);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateUD())), 30);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateRU())), 36);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateLR())), 37);
+        track.addTrackSegment(new TrackSegmentArray((tsb.generateLU())), 38);
+
 
 /*
 
